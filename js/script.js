9e0acea2d7cdba561d7d9f9d90c4eebb072a946a
Isak-JO-NTIJohanneberg <isak.jonssonohstrom@elev.ga.ntig.se>
2022-03-16 13:43:30 +0100

    fixat min hamburgermeny med javascript

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
+const nav = document.getElementById('navmeny')
+const burger = document.getElementById('burgare')
+
 function toggleMenu() {
-    nav.classList.toggle("nav_upp")
-    burgare.classList.toggle("kryss")
+    nav.classList.toggle('nav-active')
+    burger.classList.toggle('kryss')
 }
+
+burger.addEventListener('mouseover', toggleMenu)
+
+
+
+
+const undermeny = document.getElementById('undermeny')
+const maskiner = document.getElementById('maskiner')
+
+function toggleMenu2() {
+   undermeny.classList.toggle('open-unermeny')
+}
+
+maskiner.addEventListener('mouseover', toggleMenu2)
